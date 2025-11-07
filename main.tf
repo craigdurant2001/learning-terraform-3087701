4d28978ad75a25779c74d8a5d4e3c1cfc7d92a1f
craigdurant2001 <80776213+craigdurant2001@users.noreply.github.com>
2025-11-07 15:57:22 +0000

    Removed http_tcp_listeners

--- main.tf
+++ main.tf
@@ -67,14 +67,6 @@ module "alb" {
     }
   ]
 
-  http_tcp_listeners = [
-    {
-      port               = 80
-      protocol           = "HTTP"
-      target_group_index = 0
-    }
-  ]
-
   tags = {
     Environment = "dev"
   }
